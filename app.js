1282ba8a37da13e8c8aee303c36ff8237a3ac9c0
hutsuka <hutsuka99@gmail.com>
2024-03-06 10:02:20 +0900

    appjsappjs編集

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.end('<html><body><h1>Hello, world.</h1></body></html>\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
 });
